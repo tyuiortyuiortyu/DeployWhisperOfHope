cafbdb595f76529fea206a5f71a2145ee915753a
smilling <nikita.niki2410@gmail.com>
2025-07-22 13:44:41 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css', 
-                'resources/js/app.js',
-                'resources/lang/**/*.php'
+                'resources/js/app.js'
             ],
             refresh: true,
         }),
